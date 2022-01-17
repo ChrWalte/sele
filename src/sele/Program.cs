c4c1bcc4f4f97a969f6d02ee28c80d96ec253e34
Christopher Walters <chrwalte@protonmail.com>
2022-01-17 01:05:18 -0700

    initial project creation

---
+++ src/sele/Program.cs
@@ -0,0 +1,47 @@
+// imports
+using Microsoft.Extensions.FileProviders;
+
+// builder for web application
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// built web application
+var app = builder.Build();
+
+// configuration && environment properties from web application
+// development || production
+var configuration = app.Configuration;
+var environment = app.Environment;
+
+// if development environment
+if (environment.IsDevelopment())
+{
+    // use these
+    app.UseDeveloperExceptionPage();
+}
+
+// use these
+app.UseAuthorization();
+
+// disabled as https will be handled at top level
+// app.UseHttpsRedirection();
+
+// serve static files settings
+var fileLocation = configuration["ServingFiles:FileLocation"];
+var fileRequestPath = configuration["ServingFiles:RequestLocation"];
+var fileProvder = new PhysicalFileProvider(fileLocation);
+
+// enable serve static files
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = fileProvder,
+    RequestPath = fileRequestPath
+});
+
+// NOT USED NOW!
+// map controllers in \Controllers\ folder
+// app.MapControllers();
+
+// run web application
+app.Run();
