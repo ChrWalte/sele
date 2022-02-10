ba93f40fca8620b81203dfe334c45fdc02e3ca67
Christopher Walters <chrwalte@protonmail.com>
2022-02-09 17:00:12 -0700

    sele program updates for release

---
+++ src/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace sele
+{
+    public static class Constants
+    {
+        public const string ServingFileLocationEnvironmentVariable = "SERVING_FILE_LOCATION";
+        public const string ServingFileLocationConfiguration = "ServingFiles:FileLocation";
+        public const string ServingFileRequestLocationEnvironmentVariable = "SERVING_REQUEST_PATH";
+        public const string ServingFileRequestLocationConfiguration = "ServingFiles:RequestLocation";
+        public const string ServingPortNumberEnvironmentVariable = "PORT_NUMBER";
+        public const string ServingPortNumberConfiguration = "ServingFiles:PortNumber";
+        public const string ServingUnsafeTypesEnvironmentVariable = "ENABLE_UNSAFE_SERVING";
+        public const string ServingUnsafeTypesConfiguration = "ServingFiles:ServeUnsafeFileTypes";
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,18 @@
 // imports
 using Microsoft.Extensions.FileProviders;
+using sele;
 
 // builder for web application
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Logging.AddConsole();
 
-// built web application
+// build the web application
 var app = builder.Build();
 
-// configuration && environment properties from web application
-// development || production
-var configuration = app.Configuration;
-var environment = app.Environment;
-
 // if development environment
-if (environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     // use these
     app.UseDeveloperExceptionPage();
@@ -24,16 +21,49 @@ if (environment.IsDevelopment())
 // use these
 app.UseAuthorization();
 
-// disabled as https will be handled at top level
-// app.UseHttpsRedirection();
-
 // serve static files settings
-var fileLocation = configuration["ServingFiles:FileLocation"];
-var fileRequestPath = configuration["ServingFiles:RequestLocation"];
-var serveUnsafeFileTypes = bool.Parse(configuration["ServingFiles:ServeUnsafeFileTypes"]);
-var fileProvder = new PhysicalFileProvider(fileLocation);
+// environment variables take priority
+
+// file location is for physical file location on a drive
+var fileLocation = Environment.GetEnvironmentVariable(Constants.ServingFileLocationEnvironmentVariable)
+    ?? app.Configuration[Constants.ServingFileLocationConfiguration];
+
+// the request location to serve the file at: /request_location/file_name
+var fileRequestPath = Environment.GetEnvironmentVariable(Constants.ServingFileRequestLocationEnvironmentVariable)
+    ?? app.Configuration[Constants.ServingFileRequestLocationConfiguration];
+
+// the port number to expose the files on
+var portNumber = int.Parse(
+    Environment.GetEnvironmentVariable(Constants.ServingPortNumberEnvironmentVariable)
+    ?? app.Configuration[Constants.ServingPortNumberConfiguration]);
+
+// allow for unsafe serving of any file type
+var serveUnsafeFileTypes = bool.Parse(
+    Environment.GetEnvironmentVariable(Constants.ServingUnsafeTypesEnvironmentVariable)
+    ?? app.Configuration[Constants.ServingUnsafeTypesConfiguration]);
+
+// checking:
+if (fileLocation == null)
+{
+    Console.WriteLine("[FATAL]: fileLocation not provided.");
+    return;
+}
+if (fileRequestPath == null)
+{
+    Console.WriteLine("[FATAL]: fileRequestPath not provided.");
+    return;
+}
+
+// logging
+var fileCount = Directory.GetFiles(fileLocation, "*", SearchOption.AllDirectories).Length;
+Console.WriteLine($"[INFO]: Found fileLocation: {fileLocation} with {fileCount} initial file(s).");
+if (fileCount == 0) Console.WriteLine($"[WARNING]: Serving with 0 initial files.");
+Console.WriteLine($"[INFO]: Found fileRequestPath: {fileRequestPath}.");
+Console.WriteLine($"[INFO]: Found portNumber: {portNumber}.");
+Console.WriteLine($"[INFO]: Found serveUnsafeFileTypes: {serveUnsafeFileTypes}.");
 
 // enable serve static files
+var fileProvder = new PhysicalFileProvider(fileLocation);
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = fileProvder,
@@ -41,9 +71,24 @@ app.UseStaticFiles(new StaticFileOptions
     ServeUnknownFileTypes = serveUnsafeFileTypes
 });
 
-// NOT USED NOW!
 // map controllers in \Controllers\ folder
 // app.MapControllers();
 
+// add a system ping endpoint (for testing!):
+Console.WriteLine("[INFO]: Mapped: [/sele-system/ping]=>[{[sele-system]: pong!}].");
+app.MapGet("sele-system/ping", () => "{[sele-system]: pong!}");
+
+// logging:
+app.UseHttpLogging();
+
+// more logging:
+Console.WriteLine($"[INFO]: Started.");
+Console.WriteLine($"[INFO]: --- START OF ASP.NET LOGGING ---");
+
 // run web application
-app.Run();
+// on given port
+app.Run($"http://localhost:{portNumber}");
+
+// more logging:
+Console.WriteLine($"[INFO]: --- END OF ASP.NET LOGGING ---");
+Console.WriteLine($"[INFO]: Exited.");
